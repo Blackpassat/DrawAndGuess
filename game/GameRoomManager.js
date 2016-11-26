63ce265de7ee8ccdd3e18fdc579dd8fa29e6b743
Spark-Xl <sparkshenxl@gmail.com>
2016-11-26 13:13:28 +0800

    [game room] prepare game room

--- game/GameRoomManager.js
+++ game/GameRoomManager.js
@@ -2,43 +2,73 @@ var leaveGameButton = document.getElementById('leaveGame');
 var startGameButton = document.getElementById('startGame');
 var loadingPage = document.getElementById('loadingPage');
 
+const GAME_STATUS = {
+	PREPARE : "GAME_PREPARE",
+    START : "GAME_START",
+    CHANGE_PLAYER : "GAME_CHANGE_PLAYER",
+    TIMEOUT : "GAME_TIMEOUT"
+}
+
+var networkManager;
+var gameRoom;
+var clock
+
 class GameRoomManager {
 	constructor(serverAddress, roomID, myUserID) {
 		this.roomID = roomID;
 		this.myUserID = myUserID;
 		leaveGameButton.disabled = true;
 
-		var networkManager = new NetworkManager(serverAddress, roomID, myUserID);
-		networkManager.registerChannel_system(receiveSystemMessage);
+		networkManager = new NetworkManager(serverAddress, roomID, myUserID);
+		networkManager.registerChannel_system(this.receiveSystemMessage);
 
-		var gameRoom = new GameRoom(networkManager);
-		var clock = new Clock();
+		gameRoom = new GameRoom(networkManager);
+		clock = new Clock();
 
 		startGameButton.onclick = this.startGame;
-		gameRoom.prepareForGame();
+		leaveGameButton.onclick = this.leaveGame;
 	}
 
 	changeStrokeColor(colorCode) {
 		gameRoom.changeStrokeColor(colorCode);
 	}
 
-	showLoadingPage() {
-		loadingPage.style.display = 'block';
-	}
-
-	hideLoadingPage() {
-		loadingPage.style.display = 'none';
-	}
-
-	prepareGameUI() {
-		this.gameRoom.prepareForGame();
-	}
-
 	startGame() {
-		networkManager.sendData_systemMessage("Start Game!");
+		networkManager.sendData_systemMessage(GAME_STATUS.PREPARE);
+		// showLoadingPage();
+	}
+
+	leaveGame() {
+		networkManager.sendData_systemMessage(GAME_STATUS.START);
+	}
+
+	receiveSystemMessage (message) {
+		switch (message) {
+			case GAME_STATUS.PREPARE:
+				console.log("Prepare Game");
+				showLoadingPage();
+				break;
+			case GAME_STATUS.START:
+				console.log("WElcomd to the gaem");
+				hideLoadingPage();
+				prepareGameUI();
+				break;
+			default:
+				// statements_def
+				break;
+		}
 	}
 }
 
-function receiveSystemMessage (message) {
-	console.log(message);
+function showLoadingPage() {
+	loadingPage.style.display = 'block';
 }
+
+function hideLoadingPage() {
+	loadingPage.style.display = 'none';
+}
+
+function prepareGameUI() {
+	gameRoom.prepareForGame();
+}
+
